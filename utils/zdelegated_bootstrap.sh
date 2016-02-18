ea555be221ec273bca0feae851372d50afa4b5e2
Sandro Mello <sandromll@gmail.com>
2016-02-18 20:04:29 -0200

    fixing bootstrap

--- utils/zdelegated_bootstrap.sh
+++ utils/zdelegated_bootstrap.sh
@@ -11,10 +11,8 @@ fi
 
 command -v curl >/dev/null 2>&1 || { echo >&2 "Requires curl. Aborting..."; exit 1; }
 
-mkdir /usr/local/bin/zimbradelegated 2> /dev/null
+curl --silent -L https://raw.githubusercontent.com/inova-tecnologias/zimbra-tools/master/utils/zdelegated.py -o /usr/local/bin/zdelegated
+curl --silent -L https://github.com/inova-tecnologias/zimbra-tools/blob/master/utils/zrequests.py -o /usr/local/bin/zrequests.py
 
-curl --silent -L https://raw.githubusercontent.com/inova-tecnologias/zimbra-tools/master/utils/zdelegated.py -o /usr/local/bin/zimbradelegated/zdelegated
-curl --silent -L https://github.com/inova-tecnologias/zimbra-tools/blob/master/utils/zrequests.py -o /usr/local/bin/zimbradelegated/zrequests.py
-
-chmod 755 /usr/local/bin/zimbradelegated/zdelegated
+chmod 755 /usr/local/bin/zdelegated
 echo "Installation completed!"
